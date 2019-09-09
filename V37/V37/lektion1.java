6d13bf9b79000a75d8b47e03cba058e0b0ef24c6
Deltadirac <tomas.weis@ntig.se>
2019-09-09 12:58:07 +0200

    genomgång

---
+++ V37/V37/lektion1.java
@@ -0,0 +1,31 @@
+package V37;
+
+public class lektion1 {
+
+	public static void main(String[] args) {
+		// Ett sätt att skriva kommentarer
+		/* Ett annat sätt att skriva kommentarer
+		 */
+		
+		int heltal1 = 25; // skapar en låda som heter heltal1 och fyller den med ett heltal
+		int svar; // deklarera
+		
+		svar = heltal1*heltal1;
+		
+		System.out.println("Svar: " + heltal1*heltal1); // + tecknet kallas konkatinera
+		
+		double pi = 3.14;
+		int radie = 5;
+		
+		double area = pi*radie*radie;
+		
+		System.out.println("Arean är: " + area + " cm^2");
+		
+		long stortHeltal = 500000000000000000L;
+		
+		System.out.println(stortHeltal +1);
+		
+		
+	}
+
+}
