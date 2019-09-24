13598c10ec8a8ccd5d0b83c7e03a30bf3135788c
Deltadirac <tomas.weis@ntig.se>
2019-09-24 09:48:08 +0200

    task 1

---
+++ V39/loopar/firstTask.java
@@ -0,0 +1,18 @@
+package loopar;
+
+public class firstTask {
+
+	public static void main(String[] args) {
+
+		int sum = 0; // initierar en summa
+		
+		for ( int i = 0 ; i < 100 ; i++) { // kör en loop 100 gånger (0-99)
+			
+			sum = sum + i; // adderar föregående summa ihop med talet i
+		}
+		
+		System.out.println(sum); // skriver ut totala summan
+
+	}
+
+}
