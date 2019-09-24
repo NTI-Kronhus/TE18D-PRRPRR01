a0673527a4d80add45e944363c0e18c9683e5848
Deltadirac <tomas.weis@ntig.se>
2019-09-24 14:12:06 +0200

    Update firstTask.java

--- V39/loopar/firstTask.java
+++ V39/loopar/firstTask.java
@@ -9,6 +9,8 @@ public class firstTask {
 		for ( int i = 0 ; i < 100 ; i++) { // kör en loop 100 gånger (0-99)
 			
 			sum = sum + i; // adderar föregående summa ihop med talet i
+
+			System.out.println(sum);
 		}
 		
 		System.out.println(sum); // skriver ut totala summan
