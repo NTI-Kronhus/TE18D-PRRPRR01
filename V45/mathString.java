b5e8c33c5e9716864fc88953f192cf87f90ec19d
Deltadirac <tomas.weis@ntig.se>
2019-11-05 12:38:12 +0100

    genomgång tisdag

---
+++ V45/mathString.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class mathString {
+
+	public static void main(String[] args) {
+		
+		double svar = Math.sqrt((Math.pow(3, 2) + 4));
+//		System.out.println(svar);
+		
+		Math.max(3,7);
+				
+		String mening = "Tomas är dum men taskig!";
+		System.out.println(mening);
+		
+		Scanner input = new Scanner(System.in);
+		
+/*		String mening2 = input.nextLine();
+		
+		if ( mening.equals(mening2)) {
+			System.out.println("samma mening");
+		}
+	*/
+		
+		System.out.println(mening.length());
+		System.out.println("Tomas är dum men taskig!".toUpperCase());
+		System.out.println("TomAs Kan inTe StAva".toLowerCase());
+		System.out.println("Tomas är dum!".contains("TomaS"));
+		System.out.println("      TomaS är dUm     ".trim());
+		System.out.println("      TomaS är dUm     ".trim().toLowerCase());
+		System.out.println("Tomas är bra".replace('a', 'q'));
+		
+		int a = 7;
+		int b = 3;
+		
+		double answer = (double) a / (double) b;
+				
+		System.out.println(answer);
+
+	}
+
+}
